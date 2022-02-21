5f6e9a1b0730a541020bbf0ee7083b38204d9e05
valtron <valerchik91@ya.ru>
2022-02-21 12:21:45 +0300

    EL-46: code cleanup

--- microwave/src/main/java/com/example/microwave/MicrowaveApplication.java
+++ microwave/src/main/java/com/example/microwave/MicrowaveApplication.java
@@ -11,15 +11,11 @@ public class MicrowaveApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MicrowaveApplication.class.getResource("hello-view.fxml"));
-        System.out.println("fxmlLoader = " + fxmlLoader);
         Scene scene = new Scene(fxmlLoader.load(), 622, 342);
-        System.out.println("scene = " + scene);
         stage.setTitle("Microwave Application!");
         stage.setScene(scene);
         stage.setResizable(false);
-        System.out.println("scene = " + scene);
         stage.show();
-        System.out.println("scene = " + scene);
     }
 
     public static void main(String[] args) {
